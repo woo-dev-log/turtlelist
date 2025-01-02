4cc01ccbe982834716dd1c3586d6cbdcbcb44ebb
woo-dev-log <sswwhh.dev@gmail.com>
2025-01-02 16:25:49 +0900

    배포시 css 적용을 위한 코드 추가

--- front/next.config.ts
+++ front/next.config.ts
@@ -2,7 +2,11 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: 'export',
-  trailingSlash: true
+  trailingSlash: true,
+  assetPrefix:
+    process.env.NODE_ENV === "production"
+      ? "https://woo-dev-log.github.io/turtlelist/"
+      : "",
 };
 
 export default nextConfig;
